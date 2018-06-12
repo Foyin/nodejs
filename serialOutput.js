0eedc1eae26662ef43b04a35fc4a5b375eceb3f2
Foyin <Foyin>
2018-06-12 11:47:45 -0400

    Reading from arduino serial port

---
+++ serialOutput.js
@@ -0,0 +1,22 @@
+//This code uses prints out information from arduinos
+// serial port using serialport.js 
+
+var serialport = require("serialport");// version 4
+var portName = "/dev/ttyACM0"; //specify your port name
+
+var myPort = new serialport(portName, {
+    baudRate: 9600,
+    parser: new serialport.parsers.readline('\r\n')
+});
+
+myPort.on('open', function onOpen(){
+    console.log('Open connections!');
+});
+
+myPort.on('data', function onData(data){
+    console.log(data);
+});
+
+myPort.on('error', function(err) {
+  console.log('Error: ', err.message);
+})
